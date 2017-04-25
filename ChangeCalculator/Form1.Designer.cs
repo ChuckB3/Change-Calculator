90c7ad43c191080c9d72a33dd70069fbb771d42f
ChuckB3 <cbrumbaugh3@gmail.com>
2017-04-25 11:37:36 -0400

    Import project

---
+++ ChangeCalculator/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ChangeCalculator
+{
+    partial class Change_Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tenderLabel = new System.Windows.Forms.Label();
+            this.tenderBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(89, 9);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(84, 13);
+            this.priceLabel.TabIndex = 0;
+            this.priceLabel.Text = "Enter total price:";
+            this.priceLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(79, 25);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(100, 20);
+            this.priceBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tenderLabel
+            // 
+            this.tenderLabel.AutoSize = true;
+            this.tenderLabel.Location = new System.Drawing.Point(70, 48);
+            this.tenderLabel.Name = "tenderLabel";
+            this.tenderLabel.Size = new System.Drawing.Size(118, 13);
+            this.tenderLabel.TabIndex = 3;
+            this.tenderLabel.Text = "Enter amount tendered:";
+            // 
+            // tenderBox
+            // 
+            this.tenderBox.Location = new System.Drawing.Point(79, 64);
+            this.tenderBox.Name = "tenderBox";
+            this.tenderBox.Size = new System.Drawing.Size(100, 20);
+            this.tenderBox.TabIndex = 4;
+            // 
+            // Change_Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 127);
+            this.Controls.Add(this.tenderBox);
+            this.Controls.Add(this.tenderLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.priceBox);
+            this.Controls.Add(this.priceLabel);
+            this.Name = "Change_Calculator";
+            this.ShowIcon = false;
+            this.Text = "Change Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label tenderLabel;
+        private System.Windows.Forms.TextBox tenderBox;
+    }
+}
+
---
+++ ChangeCalculator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChangeCalculator
+{
+    public partial class Change_Calculator : Form
+    {
+        public double twenties;
+        public double tens;
+        public double fives;
+        public double ones;
+        public double quarters;
+        public double dimes;
+        public double nickels;
+        public double pennies;
+        public double change;
+        public double price;
+        public double tendered;
+        public double changeCount;
+
+        public Change_Calculator()
+        {
+            InitializeComponent();
+        }
+
+        public String calcChange(double n)
+        {
+            changeCount = 0;
+            twenties = Math.Floor(n / 20);
+            changeCount += (twenties*20);
+            tens = Math.Floor((n-changeCount) / 10);
+            changeCount += (tens * 10);
+            fives = Math.Floor((n - changeCount) / 5);
+            changeCount += (fives * 5);
+            ones = Math.Floor((n - changeCount) / 1);
+            changeCount += (ones * 1);
+            quarters = Math.Floor((n - changeCount) / .25);
+            changeCount += (quarters * .25);
+            dimes = Math.Floor((n - changeCount) / .10);
+            changeCount += (dimes * .10);
+            nickels = Math.Floor((n - changeCount) / .05);
+            changeCount += (nickels * .05);
+            pennies = Math.Ceiling((n - changeCount) / .01);
+            changeCount += (pennies * .01);
+
+
+            return "$20: " + twenties + "\n"
+                + "$10: " + tens + "\n"
+                + "$5: " + fives + "\n"
+                + "$1: " + ones + "\n"
+                + "$0.25: " + quarters + "\n"
+                + "$0.10: " + dimes + "\n"
+                + "$0.05: " + nickels + "\n"
+                + "$0.01: " + pennies + "\n";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            price = double.Parse(priceBox.Text);
+            tendered = double.Parse(tenderBox.Text);
+
+            if (tendered >= price)
+            {
+                change = tendered - price;
+                MessageBox.Show(calcChange(change), "Change");
+            }
+            else
+            {
+                MessageBox.Show("Customer owes $" + (price - tendered), "Error");
+            }
+
+        }
+    }
+}
